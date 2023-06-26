47c7c9bbd9619b7ac98511609dca9cbbbcf3e5a4
lanrewest <lanrewese@yahoo.com>
2023-06-26 21:27:28 +0000

    block_scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    let innerTask = true;
+    let innerTask2 = false;
+
+    task = innerTask;
+    task2 = innerTask2;
   }
 
   return [task, task2];
