6eefd0da6ed15c9c1834295229efdfa414012980
lanrewest <lanrewese@yahoo.com>
2023-07-03 22:20:42 +0000

    final_user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,15 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
-// export
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-  const results = await Promise.allSettled(promises);
-
-  const handledResults = results.map(result => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason.message
-  }));
-
-  return handledResults;
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    const arr = [];
+    for (const item of values) {
+      arr.push({ status: item.status, value: item.value || item.reason });
+    }
+    return arr;
+  });
 }
-
-
