14dc9d550d72f11df8f7305a2c491e6723b1fc19
lanrewest <lanrewese@yahoo.com>
2023-07-03 22:10:07 +0000

    not

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,6 @@
-function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
-      console.log("Got a response from the API");
-      return {
-        status: 200,
-        body: "success"
-      };
-    })
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
-
-const promise = Promise.resolve();
-handleResponseFromAPI(promise);
